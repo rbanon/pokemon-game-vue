ec1efa42c38b9299e3f6a93daecfac7bb97c889e
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-09 13:29:37 +0100

    Lógica de nombres

---
+++ src/helpers/getPokemonOptions.js
@@ -0,0 +1,25 @@
+const getPokemons = () => {
+    
+    const pokemonsArr = Array.from(Array(650))
+    
+    return pokemonsArr.map((_,index) => index+1)
+
+}
+
+const getPokemonOptions = ()=>{
+
+    const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5)
+    console.log(mixedPokemons)
+
+    getPokemonNames(mixedPokemons.splice(0,4))
+
+
+
+}
+
+
+const getPokemonNames = ([a,b,c,d]=[]) => {
+    console.log(a,b,c,d)
+
+}
+export default getPokemonOptions
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Cuál es este pokémon?</h1>
-    <PokemonPicture/>
+    <PokemonPicture :pokemonId="7" :showPokemon="true"/>
     <PokemonOptions/>
   
   </template>
@@ -8,6 +8,9 @@
   <script>
 import PokemonPicture from '@/components/PokemonPicture.vue'
 import PokemonOptions from '@/components/PokemonOptions.vue'
+import getPokemonOptions from "@/helpers/getPokemonOptions"
+
+console.log(getPokemonOptions())
   export default {
   components: {
     PokemonPicture,
