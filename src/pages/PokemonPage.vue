bb3ad81d3ef858fd214e6dbdf10302d4035189fd
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-09 13:02:56 +0100

    First commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Cuál es este pokémon?</h1>
+    <PokemonPicture/>
+    <PokemonOptions/>
+  
+  </template>
+  
+  <script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+  export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions
+    },
+  
+  }
+  </script>
